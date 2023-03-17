44d751187328cdd4f153d91354fcba490f142d6d
Kyosuke Achiwa <achiwa912@gmail.com>
2023-03-17 13:25:38 -0400

    show something

---
+++ app/auth/views.py
@@ -0,0 +1,57 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from . import auth
+from .. import db
+from ..models import User
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data.lower()).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get("next")
+            if next is None or not next.startswith("/"):
+                next = url_for("main.index")
+            return redirect(next)
+        flash("Invalid username or password.", "error")
+    return render_template("auth/login.html", form=form)
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "success")
+    return redirect(url_for("main.index"))
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You can now login.", "success")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/register.html", form=form)
+
+
+@auth.route("/change-password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash("Your password has been updated.", "success")
+            return redirect(url_for("main.index"))
+        else:
+            flash("Invalid password.", "error")
+    return render_template("auth/change_password.html", form=form)
